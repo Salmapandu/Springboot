f4a693794ce025963e5b0fdacce2ad31c1255960
Salmapandu <salmapandu281@gmail.com>
2023-06-20 11:27:53 +0300

    first commit

---
+++ src/main/java/com/example/Zanzibar/Antibiotic/usage/guide/ZanzibarAntibioticUsageGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.Zanzibar.Antibiotic.usage.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZanzibarAntibioticUsageGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZanzibarAntibioticUsageGuideApplication.class, args);
+	}
+
+}
